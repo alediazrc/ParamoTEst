22a29ac4b003028b45a0ddbd21d1ed181b367246
adiaz <adiaz@venon.solutions>
2023-03-24 12:23:39 -0300

    Testing working

--- Sat.Recruitment.Test/UnitTest1.cs
+++ Sat.Recruitment.Test/UnitTest1.cs
@@ -2,9 +2,10 @@ using System;
 using System.Dynamic;
 
 using Microsoft.AspNetCore.Mvc;
-
+using Moq;
 using Sat.Recruitment.Api.Controllers;
-
+using Sat.Recruitment.Api.Model;
+using Sat.Recruitment.Api.Services;
 using Xunit;
 
 namespace Sat.Recruitment.Test
@@ -12,12 +13,21 @@ namespace Sat.Recruitment.Test
     [CollectionDefinition("Tests", DisableParallelization = true)]
     public class UnitTest1
     {
+        private UserService userService = new UserService();
+           
         [Fact]
         public void Test1()
         {
-            var userController = new UsersController();
+            
+            User testUser = new User();
+            testUser.Name = "Mike";
+            testUser.Email = "mike@gmail.com";
+            testUser.Address = "Av. Juan G";
+            testUser.Phone = "+349 1122354215";
+            testUser.UserType = "Normal";
+            testUser.Money = decimal.Parse("124");
 
-            var result = userController.CreateUser("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
+            var result = userService.CreateUser(testUser).Result;
 
 
             Assert.Equal(true, result.IsSuccess);
@@ -27,10 +37,15 @@ namespace Sat.Recruitment.Test
         [Fact]
         public void Test2()
         {
-            var userController = new UsersController();
+            User testUser = new User();
+            testUser.Name = "Agustina";
+            testUser.Email = "Agustina@gmail.com";
+            testUser.Address = "Av. Juan G";
+            testUser.Phone = "+349 1122354215";
+            testUser.UserType = "Normal";
+            testUser.Money = decimal.Parse("124");
 
-            var result = userController.CreateUser("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
-
+            var result = userService.CreateUser(testUser).Result;
 
             Assert.Equal(false, result.IsSuccess);
             Assert.Equal("The user is duplicated", result.Errors);
